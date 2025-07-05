31e2dc45d09fd9fbd5c82f8c42938ac30baedca4
Nishant Kumar <ktanay7870@gmail.com>
2025-07-05 13:31:04 +0530

    setup test using jest

---
+++ jest.setup.js
@@ -0,0 +1,27 @@
+import '@testing-library/jest-dom'
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img {...props} />
+  },
+}))
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    }
+  },
+  useSearchParams() {
+    return new URLSearchParams()
+  },
+  usePathname() {
+    return ''
+  },
+}))
